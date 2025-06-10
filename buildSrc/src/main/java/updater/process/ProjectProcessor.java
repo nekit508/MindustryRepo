44904616a291ced398d0d746bc2c334c5e640bf3
nekit508 <83838384+nekit508@users.noreply.github.com>
2025-06-10 20:59:00 +0300

    Update ProjectProcessor.java

--- buildSrc/src/main/java/updater/process/ProjectProcessor.java
+++ buildSrc/src/main/java/updater/process/ProjectProcessor.java
@@ -141,7 +141,7 @@ sourceFolder.deleteDirectory();
         });
         RenameLocalPom.process(tmpRepository);
 
-        System.out.prinln(Vars.sources.findAll());
+        System.out.println(Vars.sources.findAll());
 
         {
             String prefix = tmpRepository.absolutePath() + "/";
